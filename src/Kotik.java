405c5606a9856f863a02485d6202a5214795c0af
Eztekto <iztekto@gmail.com>
2021-11-01 23:57:23 +0300

    Добавлен switch, case

--- src/Kotik.java
+++ src/Kotik.java
@@ -1,16 +1,40 @@
+import java.util.Random;
+
 public class Kotik {
     int prettiness;
     int weight;
     String name;
     String meow;
-    int i = 10;
     public Kotik(int prettiness,int weight, String name, String meow){
         this.prettiness = prettiness;
         this.weight = weight;
         this.name = name;
         this.meow = meow;
     }
-    void liveAnotherDay(){}
+    void liveAnotherDay(){
+        Kotik kotik1 = new Kotik(9999, 6327, "Рыжий", "Мау");
+        Random r = new Random();
+        int num = r.nextInt(5);
+        for (int i = 24; i !=0; i--) {
+            switch (num) {
+                case 0:
+                    kotik1.sleep();
+                    break;
+                case 1:
+                    kotik1.play();
+                    break;
+                case 2:
+                    kotik1.chaseMouse();
+                    break;
+                case 3:
+                    kotik1.fight();
+                    break;
+                case 4:
+                    kotik1.sayMeow();
+                    break;
+            }
+        }
+    }
 
     void eat(){
         System.out.println(name + " кушает");
@@ -19,7 +43,7 @@ public class Kotik {
         System.out.println(name + " спит");
     }
     void play(){
-        System.out.println(name + "играет с клубком");
+        System.out.println(name + " играет с клубком");
     }
     void chaseMouse(){
         System.out.println(name + " гоняет мышь");
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ class Main {
     public static void main(String args[]) {
         Kotik kotik1 = new Kotik(9999, 6327, "Рыжий", "Мау");
         Kotik kotik2 = new Kotik(9999, 3567, "Сметанка", "Мяу");
-        kotik1.sayMeow();
-        kotik2.sayMeow();
+        kotik1.liveAnotherDay();
 
     }
 }
