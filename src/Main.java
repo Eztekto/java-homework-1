b01fc7291fff2f9a0eed1858228f31b68696f734
Eztekto <iztekto@gmail.com>
2021-11-03 23:18:23 +0300

    Резервная ветка

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import main.java.animal.Kotik;
 
 class Main {
     public static void main(String args[]) {
-        Kotik kotik1 = new Kotik(9999, 6327, "Рыжий", "Мау", "хлебушек");
-        Kotik kotik2 = new Kotik(9999, 6327, "Сметанка", "Ауф", "молоко");
+        Kotik kotik1 = new Kotik(9999, 6327, "Рыжий", "Мау", "Хлебушек");
+        Kotik kotik2 = new Kotik(9999, 6327, "Сметанка", "Ауф", "Молоко");
+        kotik1.liveAnotherDay();
         kotik2.liveAnotherDay();
 
 
